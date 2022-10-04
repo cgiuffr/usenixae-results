52a89ba3ae6aff2e6be67ee430f52eaf6333fc70
Cristiano Giuffrida <giuffrida@cs.vu.nl>
2022-10-04 10:55:58 +0200

    Improve output handling

--- results.py
+++ results.py
@@ -14,27 +14,20 @@ except ImportError:
     print("Please create params.py based on params_default.py first.")
     sys.exit(1)
 
-csvout = None
-csvout_source = None
 
-
-def row_fields_to_results(row_fields, out):
+def entries_to_output(entries, out):
     if os.path.splitext(out.name)[1] == '.yaml':
-        yaml.dump([row_fields], out, sort_keys=False,
+        yaml.dump(entries, out, sort_keys=False,
                   indent=params.yaml_indent, line_break=params.out_lf, width=sys.maxsize, encoding=params.out_encoding, allow_unicode=True)
-        out.write(params.out_lf)
     else:
-        global csvout, csvout_source
-        if csvout_source != out:
-            csvout = None
-        if not csvout:
-            if params.csv_force_delimiter:
-                out.write(f'SEP={params.csv_delimiter}{params.out_lf}')
-            csvout = csv.writer(out,
-                                delimiter=params.csv_delimiter, quotechar=params.csv_quotechar, lineterminator=params.out_lf)
-            csvout_source = out
-            csvout.writerow(row_fields.keys())  # header
-        csvout.writerow(row_fields.values())
+        if params.csv_force_delimiter:
+            out.write(f'SEP={params.csv_delimiter}{params.out_lf}')
+        csvout = csv.writer(out,
+                            delimiter=params.csv_delimiter, quotechar=params.csv_quotechar, lineterminator=params.out_lf)
+        if len(entries) > 0:
+            csvout.writerow(entries[0].keys())  # header
+        for row in entries:
+            csvout.writerow(row.values())
 
 
 def row_to_paper_url(instance, row, htmls, title):
@@ -55,8 +48,8 @@ def row_to_appendix_url(instance, row):
     return str(f'{params.appendix_url_prefix}{input_pdf}')
 
 
-def row_to_results(instance, htmls, row, out):
-    row_fields = dict()
+def row_to_entry(instance, htmls, row):
+    entry = dict()
     title = row[params.output_fields['title']]
     print(f' - Processing row: {title}')
 
@@ -65,18 +58,18 @@ def row_to_results(instance, htmls, row, out):
             value = row[col].strip()
             if key == 'artifact_url' and (value == 'N/A' or value == ''):
                 continue
-            row_fields[key] = value
+            entry[key] = value
             continue
         if key == 'paper_url':
-            row_fields[key] = row_to_paper_url(instance, row, htmls, title)
+            entry[key] = row_to_paper_url(instance, row, htmls, title)
         elif key == 'appendix_url':
-            row_fields[key] = row_to_appendix_url(instance, row)
+            entry[key] = row_to_appendix_url(instance, row)
         else:
-            row_fields[key] = params.out_unknown
-    row_fields_to_results(row_fields, out)
+            entry[key] = params.out_unknown
+    return entry
 
 
-def instance_to_results(instance, htmls, out):
+def instance_to_entries(instance, htmls, entries):
     print(f'\nProcessing instance: {instance}')
     csvfilename = params.input_csv_fmt.format(instance=instance)
     csvfile = open(f'{params.input_dir}/{csvfilename}',
@@ -89,7 +82,8 @@ def instance_to_results(instance, htmls, out):
         if is_header:
             is_header = False
             continue
-        row_to_results(instance, htmls, row, out)
+        entry = row_to_entry(instance, htmls, row)
+        entries.append(entry)
     csvfile.close()
 
 
@@ -105,14 +99,17 @@ tmp = re.sub(r'lower-case\((text\(\))\)', xpatch_ci_hack,
 tmp = re.sub(r'lower-case\(([^)]*)\)', xpatch_ci_hack, tmp)
 params.paper_url_xpath_query = tmp
 
-# for each output file
+# grab entries for all the instances
+entries = []
+for instance in params.input_instances:
+    instance_htmls = htmls if params.accepted_match_all else [htmls[index]]
+    instance_to_entries(instance, instance_htmls, entries)
+    index += 1
+
+# output the entries for all the output files
 for out_file in params.output_files:
     out = open(out_file, 'w', encoding=params.in_encoding)
-    # for each instance
-    for instance in params.input_instances:
-        instance_htmls = htmls if params.accepted_match_all else [htmls[index]]
-        instance_to_results(instance, instance_htmls, out)
-        index += 1
+    entries_to_output(entries, out)
     out.close()
 
 sys.exit(0)
